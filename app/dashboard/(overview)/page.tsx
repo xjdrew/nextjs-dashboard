977309e4218fa1b0ed8c7198a51e6b61e77d3b32
xjdrew <xj.drew@gmail.com>
2024-06-28 17:47:05 +0800

    add metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,11 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+    title: 'Overview',
+};
+
 import {
     RevenueChartSkeleton,
     LatestInvoicesSkeleton,
